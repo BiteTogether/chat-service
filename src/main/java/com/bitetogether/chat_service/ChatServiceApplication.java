fdbf141e48c99767e9a65e6b6207b859fae4798f
thanhnguyen2406 <nguyencongthanh2406@gmail.com>
2026-03-27 01:55:03 +0700

    [Redis] Exclude redis configuration on repository

--- src/main/java/com/bitetogether/chat_service/ChatServiceApplication.java
+++ src/main/java/com/bitetogether/chat_service/ChatServiceApplication.java
@@ -2,8 +2,11 @@ package com.bitetogether.chat_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
+@EnableReactiveMongoRepositories(basePackages = "com.bitetogether.chat_service.repository")
 public class ChatServiceApplication {
 
   public static void main(String[] args) {
